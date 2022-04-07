6e1514cf738d8066814e8f642df9f90581c769ad
Victoria Mahoney <63237@psdschools.org>
2022-04-07 13:51:44 -0600

    done

--- Three-Letter-Word/ViewController.swift
+++ Three-Letter-Word/ViewController.swift
@@ -8,13 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var counter: Int = 0
+    var currentLetter: String!
+    
+    @IBOutlet var wordLabels: [UILabel]!
+    
+    @IBOutlet weak var currentLetterLabel: UILabel!
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+    }
+ 
+    @IBAction func whenTapped2(_ sender: Any)
+    {
+        let selectedPoint = (sender as! AnyObject).location(in: stackView)
+         print(selectedPoint)
+
+        for label in wordLabels{
+             if label.frame.contains(selectedPoint){
+                 label.text = "\(currentLetter!)"
+             }
+         }
         
+        counter += 1
+        
+        if counter == 26 {
+            counter = resetCounter()
+        }
+        getAndSetCurrentletter()
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+    }
+    
+    func getAndSetCurrentletter()
+    {
+        currentLetter = letters[counter]
+        currentLetterLabel.text = currentLetter
+    }
+
+    func resetCounter() -> Int
+    {
+        let alert = UIAlertController(title: "Resetting Counter", message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            for label in self.wordLabels {
+                label.text = "?"
+            }
+        }
+        
+        currentLetterLabel.text = "A"
+        alert.addAction(okAction)
+        present(alert, animated:true, completion:nil)
+        return 0
     }
 
 //when you do your rock paper scissors project under view there us a place called "tag" that is the index, so if you have an array you can change it to be the index of the array.
     
 }
+
+
+
+
 
